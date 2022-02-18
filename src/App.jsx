a7902f99d158a176cc1649d0b0bca151acba0ebb
divyamojas <divyamojas17@gmail.com>
2022-02-18 23:27:50 +0530

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import EntryBlock from "./EntryBlock";
-import ListBlock from "./ListBlock";
 import Menu from "./Menu";
 import "./App.css";
-import { Card } from "@mui/material";
 
 function App() {
   return (
     <div className="root">
       <Menu />
-      <Card className="mainBody">
-        <ListBlock />
-        <EntryBlock />
-      </Card>
     </div>
   );
 }
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,30 +1,22 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import "./Menu.css";
 
 function Menu() {
   return (
-    <AppBar className="root" position="sticky">
-      <Toolbar variant="dense" className="toolbar">
-        <IconButton
-          className="iconButton"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon className="icon" sx={{ fontSize: "4rem" }} />
-        </IconButton>
-        <Typography
-          className="menu__header"
-          variant="h3"
-          color="inherit"
-          component="div"
-          //   sx={{ fontFamily: "Lobster" }}
-        >
-          ToDo Online
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <div className="navbar">
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2, ml: 4 }}
+      >
+        <MenuIcon sx={{ fontSize: "2.5rem" }} />
+      </IconButton>
+      <Typography variant="h3" color="aliceblue" className="navHead">
+        ToDo Online
+      </Typography>{" "}
+    </div>
   );
 }
 
