3882c7905ae6f8e65dbb56fe6ac6f5c9dafc8799
divyamojas <divyamojas17@gmail.com>
2022-02-19 01:47:17 +0530

    Basic UI done

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,11 +1,65 @@
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from "@mui/material";
+
+import DeleteIcon from "@mui/icons-material/Delete";
 import React from "react";
 import "./TaskList.css";
+
+const TASKS = [
+  {
+    title: "Complete the website",
+    deadline: "today",
+    description: "full working website",
+  },
+  {
+    title: "Complete the website and go to party",
+    deadline: "today",
+    description: "full working website",
+  },
+  {
+    title: "Complete the website",
+    deadline: "today",
+    description: "full working website",
+  },
+  {
+    title: "Complete the website",
+    deadline: "today",
+    description: "full working website",
+  },
+  {
+    title: "Complete the website",
+    deadline: "today",
+    description: "full working website",
+  },
+];
 
 function TaskList() {
   return (
     <div className="taskList">
-      <Typography variant="h3">Pending Tasks!</Typography>
+      <Typography variant="h3" sx={{ textAlign: "center" }}>
+        Pending Tasks!
+      </Typography>
+      <div className="taskBlock">
+        {TASKS.map((el, key) => (
+          <Card sx={{ maxWidth: 345 }} className="taskItem">
+            <CardHeader
+              title={el.title}
+              subheader={`Deadline : ${el.deadline}`}
+              action={
+                <IconButton aria-label="settings" sx={{ ml: 4 }}>
+                  <DeleteIcon />
+                </IconButton>
+              }
+            />
+            <CardContent>{el.description}</CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
