785dec4b1eda1212d75d3298d4f30a9ca8348bd5
divyamojas <divyamojas17@gmail.com>
2022-02-21 15:53:28 +0530

    add basic footer block

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import "./Footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <Typography className="text" variant="h3">
+        Todo Online v0.1
+      </Typography>
+    </div>
+  );
 }
 
 export default Footer;
