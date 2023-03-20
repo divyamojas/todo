64f037285573491d89b620c4614f2f26ce00324c
Divyam <divyamojas17@gmail.com>
2023-03-20 14:20:19 +0530

    Fix refresh page not found bug

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router";
-import { auth } from "./Firebase";
+// import React, { useEffect } from "react";
+// import { useNavigate } from "react-router";
+// import { auth } from "./Firebase";
 
-export default function Root() {
-  const { currentUser } = auth;
-  const navigate = useNavigate();
-  useEffect(() => {
-    try {
-      currentUser ? navigate("/tasks") : navigate("/signin");
-      console.log("done");
-    } catch (err) {
-      console.log("err");
-      console.log(err);
-    }
-  }, [currentUser, navigate]);
-  return <div> hi</div>;
-}
+// export default function Root() {
+//   const { currentUser } = auth;
+//   const navigate = useNavigate();
+//   useEffect(() => {
+//     try {
+//       currentUser ? navigate("/tasks") : navigate("/signin");
+//       console.log("done");
+//     } catch (err) {
+//       console.log("err");
+//       console.log(err);
+//     }
+//   }, [currentUser, navigate]);
+//   return <div> hi</div>;
+// }
